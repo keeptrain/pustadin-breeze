eacb88c4418cd593950a098711b7d3e9af29fb83
Gilang <19211020@bsi.ac.id>
2025-03-17 11:51:38 +0700

    updated

--- api/index.php
+++ api/index.php
@@ -1,3 +1,19 @@
 <?php
 
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+if (str_starts_with($path, "/build/assets/")) {
+	if(str_ends_with($path, ".js")){
+	    header('Content-Type: application/javascript; charset: UTF-8');
+	    echo require __DIR__ . '/../public/' . $path;
+	    return;
+	}
+
+	if(str_ends_with($path, ".css")){
+		header("Content-type: text/css; charset: UTF-8");
+		echo require __DIR__ . '/../public/' . $path;
+		return;
+	}
+}
+
 require __DIR__ . "/../public/index.php";
